f3658a86ec708b1c63859c69708e2d19a7da4b90
Parham Alvani <parham.alvani@gmail.com>
2015-01-24 02:25:08 +0330

    message struct improved.

--- connection-server/src/message.h
+++ connection-server/src/message.h
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 30-12-2014
  *
- * [] Last Modified : Sat Jan 24 02:12:32 2015
+ * [] Last Modified : Sat Jan 24 02:24:33 2015
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -16,27 +16,30 @@
 #define MESSAGE_SIZE (255 * 4 + 32)
 
 /*
- * message struct size must be less than
+ * message header struct size must be less than
  * 4 * 255 byte
  * @verb: send join leave
- *
- * @arg: if verb has no argument it must be " "
  *
  * @group_id: if verb has no group id it must be -1
  *
  * @client_name: username of client
+ *
+ * @size: sizeof message body in bytes
+ *
  * +------------+------------+-------------+------------+
- * |
- * |
- * |
+ * |    verb    |  group_id  | client_name |     size   |
  * +------------+------------+-------------+------------+
+ * |                                                    |
+ * |                  Message Body                      |
+ * |                                                    |
+ * +----------------------------------------------------+
 */
 
 typedef struct{
 	char verb[255];
-	char arg[255];
 	int group_id;
 	char client_name[255];
+	int m_size;
 } Message;
 
 int serialize_message(char *buffer, const Message *message);
