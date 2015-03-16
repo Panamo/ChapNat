9762c4beccd301a6bd8b8b28e890c5a30efcc0bd
Parham Alvani <parham.alvani@gmail.com>
2015-03-16 17:20:26 +0330

    project structure changed.

---
+++ connection/resolve.c
@@ -0,0 +1,12 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : resolve.c
+ *
+ * [] Creation Date : 16-03-2015
+ *
+ * [] Last Modified : Mon 16 Mar 2015 04:47:57 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
---
+++ connection/resolve.h
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : resolve.h
+ *
+ * [] Creation Date : 16-03-2015
+ *
+ * [] Last Modified : Mon 16 Mar 2015 04:50:19 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#ifndef RESOLVE_H
+#define RESOLVE_H
+
+
+
+#endif
