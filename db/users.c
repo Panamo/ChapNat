a7086b2c263c794a917c829c45460a15713d80b3
Parham Alvani <parham.alvani@gmail.com>
2015-03-20 15:05:32 +0330

    getUser function is added into users.c

--- db/users.c
+++ db/users.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 25-01-2015
  *
- * [] Last Modified : Fri 20 Mar 2015 12:26:02 AM IRST
+ * [] Last Modified : Fri 20 Mar 2015 03:05:27 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -43,4 +43,21 @@ void add_user(const struct message *message, int socket)
 
 void get_user(const struct message *message, int socket)
 {
+	struct message replay;
+	char *query;
+  	unsigned char hashpas[SHA_DIGEST_LENGTH];
+
+    	SHA1(message->pass, sizeof(message->pass) - 1, hashpas);
+	
+	asprintf(&query, "SELECT * FROM users WHERE username='%s' and password='%s';", message->user, hashpas);
+	ulog("%s", query);
+	free(query);
+
+	strcpy(replay.verb, "st00");
+	strcpy(replay.user_id, "0");
+	strcpy(replay.user, "-");
+	strcpy(replay.pass, "-");
+
+	send_message(&replay, socket);
+
 }
