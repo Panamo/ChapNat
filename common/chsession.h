e686944bac9204a126aa2db8d4c65d180fa38340
Parham Alvani <parham.alvani@gmail.com>
2015-03-25 14:37:03 +0430

    chsession object definition completed.

--- common/chsession.h
+++ common/chsession.h
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 25-03-2015
  *
- * [] Last Modified : Wed 25 Mar 2015 01:44:49 PM IRDT
+ * [] Last Modified : Wed 25 Mar 2015 02:36:56 PM IRDT
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -14,7 +14,31 @@
 #define CHSESSION_H
 
 struct chsession {
-	
+	struct chevent *head;
+	void (*cleaner)(struct chevent *event);
 };
+
+struct chsession *chsession_new(void);
+
+/*
+ * for all event, this function first use cleaner@chsession
+ * freeing user_data then free chevent itself
+*/
+void chsession_free(struct chsession *session);
+
+void chsession_register_cleaner(struct chsession *session,
+		void (*cleaner)(struct chevent *event));
+
+void chsession_add_event(struct chsession *session,
+		const struct chevent *event);
+
+/* TODO */
+void chsession_remove_event(struct chsession *session,
+		const struct chevent *event);
+
+void chsession_dispatcher(const struct chsession *session,
+		const void *data,
+		const struct chmessage *message,
+		const void *user_data);
 
 #endif
