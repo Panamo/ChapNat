e10f85e3b6527d91d5d10ce3f4d2ccd67a65ff5b
Parham Alvani <parham.alvani@gmail.com>
2015-03-27 00:23:33 +0430

    sgup event is added.

--- db/events.c
+++ db/events.c
@@ -5,28 +5,75 @@
  *
  * [] Creation Date : 26-03-2015
  *
- * [] Last Modified : Thu 26 Mar 2015 11:06:19 PM IRDT
+ * [] Last Modified : Fri 27 Mar 2015 12:23:18 AM IRDT
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "../common/chmessage.h"
+#include "../common/chevent.h"
+#include "../common/chsession.h"
 
 #include "message.h"
 #include "users.h"
 #include "events.h"
+#include "asprintf.h"
 
-void sgin_command(const struct message *message, int socket_fd)
+static void sgin_handler(const struct message *message, int socket_fd)
 {
 }
 
-void sgup_command(const struct message *message, int socket_fd)
+static void sgup_handler(const struct chmessage *chmessage, const void *user_data)
 {
-	add_user(message, socket_fd);
+	const struct message *message;
+	const int* socket_fd;
+	
+	socket_fd = (const int *) user_data;
+	message = (const struct message *) chmessage;
+	add_user(message, *socket_fd);
 }
 
-void newg_command(const struct message *message, int socket_fd)
+static void newg_handler(const struct message *message, int socket_fd)
 {
 }
 
+static void event_cleaner(struct chevent *event)
+{
+	free(event->data);
+}
+
+static int event_dispatcher(const struct chevent *event,
+		const void *data)
+{
+	const char *event_in_name;
+	const char *event_name;
+	
+	event_in_name = (const char *) data;
+	event_name = (const char *) event->data;
+	if (!strcmp(event_in_name, event_name))
+		return 1;
+	return 0;
+}
+
+struct chsession *init_events(void)
+{
+	struct chsession *session;
+	char *sgup_id;
+	struct chevent *sgup;
+
+	session = chsession_new();
+	chsession_register_cleaner(session, event_cleaner);
+	chsession_register_dispatcher(session, event_dispatcher);
+
+	asprintf(&sgup_id, "sgup");
+	sgup = chevent_new();
+	chevent_register_handler(sgup, sgup_handler);
+	chevent_register_data(sgup, sgup_id);
+	chsession_add_event(session, sgup);
+
+	return session;
+}
--- db/events.h
+++ db/events.h
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 26-03-2015
  *
- * [] Last Modified : Thu 26 Mar 2015 11:04:12 PM IRDT
+ * [] Last Modified : Thu 26 Mar 2015 11:21:05 PM IRDT
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -13,6 +13,6 @@
 #ifndef EVENTS_H
 #define EVENTS_H
 
-const chsession *init_events(void);
+struct chsession *init_events(void);
 
 #endif
