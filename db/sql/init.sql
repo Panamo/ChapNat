da7cf59bb6ede0afbbcae2b121fb55c209d23064
Parham Alvani <parham.alvani@gmail.com>
2016-04-09 07:14:21 +0430

    Database initiation script was added.

---
+++ db/sql/init.sql
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : init.sql
+ *
+ * [] Creation Date : 10-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+CREATE USER ChapNat WITH PASSWORD '1234';
+
+CREATE DATABASE ChapNat;
+
+GRANT ALL PRIVILEGES ON DATABASE chapnat TO chapnat;
