af4fd369b756ff9e62e03bb46bdd003badb8fe5b
Parham Alvani <parham.alvani@gmail.com>
2016-04-10 06:36:50 +0430

    Reset SQL script was added.

---
+++ db/sql/reset.sql
@@ -0,0 +1,17 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : reset.sql
+ *
+ * [] Creation Date : 11-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+DROP DATABASE chapnat;
+
+DROP USER chapnat;
