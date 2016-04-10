36deb67474e32e73a405559ff331ccc51ea54ea1
Parham Alvani <parham.alvani@gmail.com>
2016-04-10 06:42:20 +0430

    Database populating script was added.

---
+++ db/sql/build.sql
@@ -0,0 +1,16 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : build.sql
+ *
+ * [] Creation Date : 13-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+
+\copy users FROM 'data/users.txt';
